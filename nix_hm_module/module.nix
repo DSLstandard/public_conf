dbb641cc81c683083d04af5e3c7dfa2d3dd1fda9
DSLstandard <qgeneral35@gmail.com>
2024-05-30 19:58:01 +0800

    add tmux and vscode

---
+++ nix_hm_module/module.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfgName = "dslStandardConfig";
+  cfg = config.${cfgName};
+in
+{
+  options.${cfgName} = {
+    configureTmux = lib.mkEnableOption "Set tmux conf";
+    configureVscode = lib.mkEnableOption "Set vscode key bindings + add vscodevim";
+  };
+
+  config = lib.mkMerge [
+    (
+      lib.mkIf cfg.configureTmux {
+        home.packages = [ pkgs.xclip ];
+        programs.tmux = {
+          extraConfig = builtins.readFile ./files/tmux.conf;
+        };
+      }
+    )
+    (
+      lib.mkIf cfg.configureVscode {
+        programs.vscode = {
+          extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+          ];
+          keybindings = builtins.fromJSON (builtins.readFile ./files/vscode_keybindings.json);
+        };
+      }
+    )
+    (
+      lib.mkIf cfg.configureHaskeline {
+        programs.vscode = {
+          extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+          ];
+          keybindings = builtins.fromJSON (builtins.readFile ./files/vscode_keybindings.json);
+        };
+      }
+    )
+  ];
+}
